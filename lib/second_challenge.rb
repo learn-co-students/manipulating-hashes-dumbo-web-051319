3d27b310d5996286fe171e34f98b467d0f395b10
“hannah6530” <hannah6530@gmail.com>
2019-05-13 19:42:02 -0400

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,4 @@
+
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -7,7 +8,9 @@ def second_challenge
   }
 
   #code your solution here!
-
-  
-
+  food = []
+  groceries.each  do |key, values|
+      food << key.values
+    end
+  food
 end
